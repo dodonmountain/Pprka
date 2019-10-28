9522adb007baa7bc1b6679e0fe25e2b06ae62eca
dodonmountain <lkh151515@gmail.com>
2019-10-28 21:54:12 +0900

    kh| curation-list

--- Pprka/settings.py
+++ Pprka/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'indexes',
     # 'payments',
-    'accounts',
+    # 'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
