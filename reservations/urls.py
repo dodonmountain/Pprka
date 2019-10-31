e68e20d8e0813a12f5145d6359826634923877dc
JiminLee411 <wlals41189@gmail.com>
2019-10-31 17:57:20 +0900

    LJM | reservations - Add form, templates (ing)

--- reservations/urls.py
+++ reservations/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'reservations'
 
 urlpatterns = [
-    path('', view.index, name='index'),
-    
+    path('', views.index, name='index'),
+
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
+from .forms import ReservationForm
 
 # Create your views here.
 def index(request):
-    
+    reservation_form = ReservationForm()
+    context = {
+        'reservation_form': reservation_form
+    }
+    return render(request, 'reservations/index.html', context)
